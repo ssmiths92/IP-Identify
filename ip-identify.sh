8fcd42636b1a7e7131b5df93424cfaf2035a7154
sam <ssmiths92@gmail.com>
2017-02-12 18:37:44 -0500

    Clean up file names, change final output to only IPs

--- ip-identify.sh
+++ ip-identify.sh
@@ -4,18 +4,18 @@
 
 cut -f 1 -d " " log.txt | sort | uniq > linodeips1.txt
 
-# Runs whois on our IPs, then greps for and cats only the "NetName" field.
+# Runs whois on our IPs, then greps for and cats only the "NetName" field. Cut and leave only the second field, which is the NetName value.
 
-cat linodeips1.txt | awk '{system("whois "$1)}' | grep NetName: | cut -f 2 -d ":" > linodeips3.txt
+cat linodeips1.txt | awk '{system("whois "$1)}' | grep NetName: | cut -f 2 -d ":" > linodeips2.txt
 
 # Merges the IP addresses and NetNames into one file, linodeips.txt.
 
-paste -d' ' linodeips1.txt linodeips3.txt > linodeips.txt
+paste -d' ' linodeips1.txt linodeips2.txt > linodeips.txt
 
 # Removes unecessary files.
 
-rm linodeips1.txt linodeips3.txt
+rm linodeips1.txt linodeips2.txt
 
 # Cats the contents of linodeips.txt and greps for Linode IPs
 
-cat linodeips.txt | grep 'LINODE-US'
+cat linodeips.txt | grep 'LINODE-US' | cut -f 1 -d " "
